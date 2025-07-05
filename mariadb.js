628ed7c7028a1bd02a4e90ad2a7ebfbce1ed8fd9
bsu0404 <bsu0404@naver.com>
2025-07-05 17:04:55 +0900

    fix: Cloudtype 외부 접속을 위해 MariaDB 포트 옵션 추가

--- mariadb.js
+++ mariadb.js
@@ -12,6 +12,7 @@ const mariadb = require("mysql2/promise");
 
 const connection = mariadb.createConnection({
   host: process.env.HOST,
+  port: process.env.PORT_DB,
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
